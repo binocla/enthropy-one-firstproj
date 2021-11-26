75e21c7ee32db59dd82ce8f5a1cc208de02887f0
cbman1999 <38330163+cbman1999@users.noreply.github.com>
2021-11-27 02:13:51 +0300

    maybe done

--- src/main/java/space/enthropy/models/Universe.java
+++ src/main/java/space/enthropy/models/Universe.java
@@ -1,7 +1,11 @@
 package space.enthropy.models;
 
+import java.time.LocalDate;
+
 public class Universe {
-    /* todo
-        Fields here
-     */
+    public Timeline timeline;
+    public LocalDate created;
+    public String title;
+    public Long interstellarIdentifier;
+    public String blackHoleEntry;
 }
